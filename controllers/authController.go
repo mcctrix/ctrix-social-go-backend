f14a2a0c82b82569c5ceb09c0107d801f58b5555
Chandrashekhar Kachawa <42296139+mcctrix@users.noreply.github.com>
2024-06-30 18:44:59 +0530

    removed unnessary cors code

--- controllers/authController.go
+++ controllers/authController.go
@@ -7,8 +7,6 @@ import (
 func Login() func(g *gin.Context) {
 
 	return func(g *gin.Context) {
-		g.Header("Access-Control-Allow-Origin", "*")
-		g.Header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH,OPTIONS")
 
 		g.JSON(200, gin.H{
 			"test": "working",
