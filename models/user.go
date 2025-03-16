54864212a423538f673a9dd94362dd539916fcf5
ctrix <csk77@pm.me>
2025-03-16 16:08:56 +0530

    added json tags for models

--- models/user.go
+++ models/user.go
@@ -22,48 +22,48 @@ type User_Profile struct {
 }
 
 type User_Additional_Info struct {
-	Id              string   `json:"id"`
-	Hobbies         []string `json:"hobbies"`
-	Family_members  []string
-	Relation_status string
-	Dob             time.Time
-	Bio             string
-	Gender          string
+	Id              string    `json:"id"`
+	Hobbies         []string  `json:"hobbies"`
+	Family_members  []string  `json:"family_members"`
+	Relation_status string    `json:"relation_status"`
+	Dob             time.Time `json:"dob"`
+	Bio             string    `json:"bio"`
+	Gender          string    `json:"gender"`
 }
 
 type User_Settings struct {
-	Id          string
-	Hide_post   []string
-	Hide_story  []string
-	Block_user  []string
-	Show_online bool
+	Id          string   `json:"id"`
+	Hide_post   []string `json:"hide_post"`
+	Hide_story  []string `json:"hide_story"`
+	Block_user  []string `json:"block_user"`
+	Show_online bool     `json:"show_online"`
 }
 
 type User_Data struct {
-	Id      string
-	Posts   []string
-	Stories []string
-	Notes   []string
+	Id      string   `json:"id"`
+	Posts   []string `json:"posts"`
+	Stories []string `json:"stories"`
+	Notes   []string `json:"notes"`
 }
 
 type User_Posts struct {
-	Id                string
-	Creator_id        string
-	Created_at        time.Time
-	Group_id          string
-	Text_content      string
-	Pictures_attached []string
-	Liked_by          []string
-	Comments          []string
+	Id                string    `json:"id"`
+	Creator_id        string    `json:"creator_id"`
+	Created_at        time.Time `json:"creator_at"`
+	Group_id          string    `json:"group_id"`
+	Text_content      string    `json:"text_content"`
+	Pictures_attached []string  `json:"pictures_attached"`
+	Liked_by          []string  `json:"liked_by"`
+	Comments          []string  `json:"comments"`
 }
 
 type User_post_Comments struct {
-	Id                string
-	Post_id           string
-	Creator_id        string
-	Created_at        time.Time
-	Content           string
-	Pictures_attached []string
-	Nested_comments   []string
-	Liked_by          []string
+	Id                string    `json:"id"`
+	Post_id           string    `json:"post_id"`
+	Creator_id        string    `json:"creator_id"`
+	Created_at        time.Time `json:"created_at"`
+	Content           string    `json:"content"`
+	Pictures_attached []string  `json:"pictures_attached"`
+	Nested_comments   []string  `json:"nested_comments"`
+	Liked_by          []string  `json:"liked_by"`
 }
